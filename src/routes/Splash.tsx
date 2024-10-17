cc40ff2df021f3811ff15718e95303e355aac1a4
밤밤 <jiiyo0@naver.com>
2024-10-17 19:10:03 +0900

    Feat: splash 애니메이션

--- src/routes/Splash.tsx
+++ src/routes/Splash.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+import Logo from "../assets/images/logo-txt-reverse.svg";
+import Symbol from "../assets/images/logo-symbol.svg";
+
+const flipAnimation = keyframes`
+  0% {
+    transform: scaleX(1) scaleY(1);
+  }
+  50% {
+    transform: scaleX(-1) scaleY(1);
+  }
+  100% {
+    transform: scaleX(-1) scaleY(-1);
+  }
+`;
+
+const LogoImage = styled.img`
+	animation: ${flipAnimation} 2s forwards;
+`;
+
+const Container = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+`;
 
 export default function Splash() {
-  return <div>Splash</div>;
+	return (
+		<Container>
+			<LogoImage src={Logo} alt="Logo" />
+		</Container>
+	);
 }
