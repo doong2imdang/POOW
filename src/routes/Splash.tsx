264b997d30c0abc5129fe90a08d75a488c2c71e0
밤밤 <jiiyo0@naver.com>
2024-10-17 21:01:04 +0900

    Feat: 스플래시 화면 구현

--- src/routes/Splash.tsx
+++ src/routes/Splash.tsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
-import Logo from "../assets/images/logo-txt-reverse.svg";
+import Logo from "../assets/images/logo-txt-dark.svg";
 import Symbol from "../assets/images/logo-symbol.svg";
 
 const flipAnimation = keyframes`
   0% {
-    transform: scaleX(1) scaleY(1);
+    transform: scaleX(-1) scaleY(-1);
   }
   50% {
-    transform: scaleX(-1) scaleY(1);
+    transform: scaleX(1) scaleY(-1);
   }
   100% {
-    transform: scaleX(-1) scaleY(-1);
+    transform: scaleX(1) scaleY(1);
   }
 `;
 
 const LogoImage = styled.img`
-	animation: ${flipAnimation} 2s forwards;
+	animation: ${flipAnimation} 1.5s forwards;
+`;
+
+const SymbolImage = styled.img`
+	position: absolute;
+	top: calc(50% -20px);
+	left: 50%;
+	transform: translate(-50%, -100%);
+	opacity: 0;
+	transition: opacity 0.5s ease-in-out;
+	&.visible {
+		opacity: 1;
+	}
 `;
 
 const Container = styled.div`
@@ -24,12 +36,28 @@ const Container = styled.div`
 	justify-content: center;
 	align-items: center;
 	height: 100vh;
+	position: relative;
 `;
 
 export default function Splash() {
+	const [showSymbol, setShowSymbol] = useState(false);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setShowSymbol(true);
+		}, 1000);
+
+		return () => clearTimeout(timer);
+	}, []);
+
 	return (
 		<Container>
 			<LogoImage src={Logo} alt="Logo" />
+			<SymbolImage
+				src={Symbol}
+				alt="Symbol"
+				className={showSymbol ? "visible" : ""}
+			/>
 		</Container>
 	);
 }
