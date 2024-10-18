505107d84aeee27d68027014f410e7687a505d8b
KIm Seo Young <jmh7367@naver.com>
2024-10-18 22:40:55 +0900

    Design: 버튼 컴포넌트 골격 완성

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,52 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Button() {
-  return <div>Button</div>;
+interface ButtonProps {
+  background?: string;
+  width?: string;
+  height?: string;
+  color?: string;
+  type?: "button" | "submit" | "reset";
+  text?: string;
+  disabled?: boolean;
 }
+
+const Button: React.FC<ButtonProps> = ({
+  background = "transparent",
+  width = "90px",
+  height = "32px",
+  color = "var(--color-dark)",
+  type = "button",
+  text = "저장",
+  disabled = true,
+}) => {
+  return (
+    <ButtonStyle
+      background={background}
+      width={width}
+      height={height}
+      color={color}
+      type={type}
+      disabled={disabled}
+    >
+      {text}
+    </ButtonStyle>
+  );
+};
+
+const ButtonStyle = styled.button<ButtonProps>`
+  background-color: ${(props) => props.background};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  color: ${(props) => props.color};
+  border-radius: 20px;
+  font-weight: bold;
+  font-size: 14px;
+
+  ${(props) =>
+    props.disabled
+      ? `background-color: var(--color-disabled); color: #fff; pointer-events: none; `
+      : `background-color: var(--color-main); color: var(--color-dark); `}
+`;
+
+export default Button;
