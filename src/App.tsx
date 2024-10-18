e8d4192076f1baa7f5890be5a113bc5a3a979a8c
밤밤 <jiiyo0@naver.com>
2024-10-18 22:58:15 +0900

    Refactor: 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -21,72 +21,72 @@ import reset from "styled-reset";
 import styled from "styled-components";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "profile",
-        element: <Profile />,
-      },
-      {
-        path: "setprofile",
-        element: <SetProfile />,
-      },
-      {
-        path: "checklist",
-        element: <CheckList />,
-      },
-      {
-        path: "setchecklist",
-        element: <SetCheckList />,
-      },
-      {
-        path: "editprofile",
-        element: <EditProfile />,
-      },
-      {
-        path: "mood",
-        element: <Mood />,
-      },
-      {
-        path: "setmood",
-        element: <SetMood />,
-      },
-      {
-        path: "schedule",
-        element: <Schedule />,
-      },
-      {
-        path: "setschedule",
-        element: <SetSchedule />,
-      },
-      {
-        path: "search",
-        element: <Search />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/splash",
-    element: <Splash />,
-  },
-  {
-    path: "/error",
-    element: <Error />,
-  },
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "",
+				element: <Home />,
+			},
+			{
+				path: "profile",
+				element: <Profile />,
+			},
+			{
+				path: "setprofile",
+				element: <SetProfile />,
+			},
+			{
+				path: "checklist",
+				element: <CheckList />,
+			},
+			{
+				path: "setchecklist",
+				element: <SetCheckList />,
+			},
+			{
+				path: "editprofile",
+				element: <EditProfile />,
+			},
+			{
+				path: "mood",
+				element: <Mood />,
+			},
+			{
+				path: "setmood",
+				element: <SetMood />,
+			},
+			{
+				path: "schedule",
+				element: <Schedule />,
+			},
+			{
+				path: "setschedule",
+				element: <SetSchedule />,
+			},
+			{
+				path: "search",
+				element: <Search />,
+			},
+		],
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/signup",
+		element: <Signup />,
+	},
+	{
+		path: "/splash",
+		element: <Splash />,
+	},
+	{
+		path: "/error",
+		element: <Error />,
+	},
 ]);
 
 const GlobalStyles = createGlobalStyle`
@@ -130,32 +130,32 @@ background: white;
 `;
 
 const Container = styled.div`
-  width: 390px;
-  height: 100vh;
-  margin: 0 auto;
-  background: white;
+	width: 390px;
+	height: 100vh;
+	margin: 0 auto;
+	background: white;
 `;
 
-function App() {
-  const [splashVisible, setSplashVisible] = useState(true);
+const App: React.FC = () => {
+	const [splashVisible, setSplashVisible] = useState<boolean>(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setSplashVisible(false);
-    }, 2000);
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setSplashVisible(false);
+		}, 2000);
 
-    return () => clearTimeout(timer);
-  }, []);
+		return () => clearTimeout(timer);
+	}, []);
 
-  return (
-    <>
-      <GlobalStyles />
-      <Container>
-        {splashVisible ? <Splash /> : ""}
-        <RouterProvider router={router} />
-      </Container>
-    </>
-  );
-}
+	return (
+		<>
+			<GlobalStyles />
+			<Container>
+				{splashVisible ? <Splash /> : null}
+				<RouterProvider router={router} />
+			</Container>
+		</>
+	);
+};
 
 export default App;
