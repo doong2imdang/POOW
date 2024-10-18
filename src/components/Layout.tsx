4ab5ae09891f0af5b40053dd1a94a3fdb962fd72
KIm Seo Young <jmh7367@naver.com>
2024-10-18 22:39:36 +0900

    Style: 불필요한 코드 삭제

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
 import NavBar from "./NavBar";
 
 export default function Layout() {
   return (
     <>
-      <Header />
       <Outlet />
       <NavBar />
     </>
