5e0d3e7f027c5830fc7ee77b42a0f64220aa87dc
KIm Seo Young <jmh7367@naver.com>
2025-01-06 23:30:31 +0900

    Style: css 수정

--- src/components/MyMood.tsx
+++ src/components/MyMood.tsx
@@ -57,7 +57,7 @@ export default function MyMood() {
   );
 }
 
-const MyMoodStyle = styled.div`
+export const MyMoodStyle = styled.div`
   background: #fff;
   display: flex;
   flex-direction: column;
@@ -85,9 +85,10 @@ const PostDisplayToggle = styled.div`
   }
 `;
 
-const MoodList = styled.ul`
+export const MoodList = styled.ul`
   font-size: 14px;
-  padding: 0 29px 60px 29px;
+  margin: 0 auto;
+  padding-bottom: 60px;
 
   li {
     border-bottom: 1px solid var(--color-disabled);
@@ -97,6 +98,7 @@ const MoodList = styled.ul`
 
     > button {
       margin-left: auto;
+      transform: translateX(8px);
     }
 
     > img {
@@ -106,6 +108,10 @@ const MoodList = styled.ul`
       border-radius: 10px;
       border: 1px solid #dbdbdb;
       margin: 16px 0;
+    }
+
+    > span {
+      width: 326px;
     }
 
     p {
