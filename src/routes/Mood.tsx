1997ae4b9350339d266791fd45d0d484bbb31e13
KIm Seo Young <jmh7367@naver.com>
2025-03-26 11:46:25 +0900

    Feat: MyMood페이지에서 데이터 받기

--- src/routes/Mood.tsx
+++ src/routes/Mood.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Mood() {
-  return <div>Mood</div>;
+  const location = useLocation();
+  const mood = location.state;
+
+  if (!mood) {
+    return <p>데이터가 없습니다.</p>;
+  }
+
+  return (
+    <div>
+      <h2>{mood.id}</h2>
+    </div>
+  );
 }
