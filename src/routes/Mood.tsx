0f367c724b50985f2db56ac851d5eec69c352f76
KIm Seo Young <jmh7367@naver.com>
2025-03-31 11:13:43 +0900

    Feat: 뒤로가기 기능만 있는 Header 컴포넌트 생성 후 적용

--- src/routes/Mood.tsx
+++ src/routes/Mood.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function Mood() {
   const location = useLocation();
   const mood = location.state;
+
+  const [isBottomSheet, setIsBottomSheet] = useState<boolean>(false);
+  const toggleBottomSheet = () => {
+    setIsBottomSheet((prev) => !prev);
+  };
 
   if (!mood) {
     return <p>데이터가 없습니다.</p>;
   }
 
   return (
-    <div>
+    <>
+      <Header back />
       <h2>{mood.id}</h2>
-    </div>
+    </>
   );
 }
